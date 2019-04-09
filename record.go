8a55348c0ac642383e7bf1146eb2b50773806bdf
clem-42 <clem@clem.org>
2019-04-09 22:06:53 +0200

    handle update and expiration date (#2)
    Handle update and expiration date
    
    
    Co-Authored-By: clem-42 <clem@clem.org>

--- record.go
+++ record.go
@@ -7,18 +7,18 @@ import (
 	"github.com/jinzhu/now"
 )
 
-var domainRegistrationKeys = []string{
-	"created",
-	"creation date",
-	"changed",
-	"domain create date",
-	"registered on",
+var domainRegistrationKeys = map[string][]string{
+	"create": {"created", "creation date", "domain create date", "registered on"},
+	"update": {"changed", "updated date", "last-update"},
+	"expire": {"registry expiry date", "expiry date"},
 }
 
 // Record holds the information returned by the whois server
 type Record struct {
 	Domain     string
 	CreatedOn  *time.Time
+	UpdatedOn  *time.Time
+	ExpiresOn  *time.Time
 	Registered bool
 	Status     string
 }
@@ -27,24 +27,28 @@ type Record struct {
 func NewRecord(domain string, keyValues map[string]string) *Record {
 	record := &Record{Domain: domain}
 	for key, value := range keyValues {
-		if isValidRegistrationKey(key) {
-			if parsedDate, err := now.Parse(value); err == nil {
-				record.CreatedOn = &parsedDate
-				record.Registered = true
+		for kind, regValues := range domainRegistrationKeys {
+			for _, regValue := range regValues {
+				if strings.ToLower(key) == regValue {
+					if parsedDate, err := now.Parse(value); err == nil {
+						switch kind {
+						case "create":
+							record.CreatedOn = &parsedDate
+						case "update":
+							record.UpdatedOn = &parsedDate
+						case "expire":
+							record.ExpiresOn = &parsedDate
+						}
+						record.Registered = true
+					}
+				}
 			}
-		} else if key == "Domain Status" {
+		}
+
+		if key == "Domain Status" {
 			record.Status = value
 		}
 	}
 
 	return record
-}
-
-func isValidRegistrationKey(key string) bool {
-	for _, validKey := range domainRegistrationKeys {
-		if strings.ToLower(key) == validKey {
-			return true
-		}
-	}
-	return false
 }
